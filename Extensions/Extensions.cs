4ae04646ed4851751cd7ee8ae83e58ab0bf07eac
William <williamlangroudi@gmail.com>
2018-08-24 09:39:07 +0100

    Add Create Pagination helper Add Proposal Params for setting paging values through the request

--- Extensions/Extensions.cs
+++ Extensions/Extensions.cs
@@ -1,7 +1,17 @@
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using ZestMonitor.Api.Helpers;
+
 namespace ZestMonitor.Api.Extensions
 {
-    public class Extensions
+    public static class Extensions
     {
-        
+        public static void AddPagination(this HttpResponse response, int currentPage,
+        int itemsPerPage, int totalItems, int totalPages)
+        {
+            var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
+            response.Headers.Add("Pagination", JsonConvert.SerializeObject(paginationHeader));
+            response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+        }
     }
 }
---
+++ Helpers/ProposalParams.cs
@@ -0,0 +1,15 @@
+namespace ZestMonitor.Api.Helpers
+{
+    public class ProposalParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 10;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+
+    }
+}
